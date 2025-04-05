739da107ffe7f8bc9c61707c01d2664e454992b0
=mauricio <Mauricioarango.cm@gmail.com>
2025-04-04 18:25:39 -0600

    actividad peliculas

---
+++ javascript/movies.js
@@ -0,0 +1,113 @@
+movies = [
+    {
+      plot: 'Cartoon figures announce, via comic strip balloons, that they will move - and move they do, in a wildly exaggerated style.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BYzg2NjNhNTctMjUxMi00ZWU4LWI3ZjYtNTI0NTQxNThjZTk2XkEyXkFqcGdeQXVyNzg5OTk2OA@@._V1_SY1000_SX677_AL_.jpg',
+      title: 'Winsor McCay, the Famous Cartoonist of the N.Y. Herald and His Moving Comics'
+    },
+    {
+      plot: 'The cartoonist, Winsor McCay, brings the Dinosaurus back to life in the figure of his latest creation, Gertie the Dinosaur.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BMTQxNzI4ODQ3NF5BMl5BanBnXkFtZTgwNzY5NzMwMjE@._V1_SY1000_SX677_AL_.jpg',
+      title: 'Gertie the Dinosaur'
+    },
+    {
+      plot: "Young Pauline is left a lot of money when her wealthy uncle dies. However, her uncle's secretary has been named as her guardian until she marries, at which time she will officially take ...",
+      poster: 'https://m.media-amazon.com/images/M/MV5BMzgxODk1Mzk2Ml5BMl5BanBnXkFtZTgwMDg0NzkwMjE@._V1_SY1000_SX677_AL_.jpg',
+      title: 'The Perils of Pauline'
+    },
+    {
+      plot: 'A con artist masquerades a Russian nobility and attempts to seduce the wife of an American diplomat.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BMTk2NDkxMTY1Nl5BMl5BanBnXkFtZTgwNDI1NDU5MTE@._V1_SY1000_SX677_AL_.jpg',
+      title: 'Foolish Wives'
+    },
+    {
+      plot: "Gwen's family is rich, but her parents ignore her and most of the servants push her around, so she is lonely and unhappy. Her father is concerned only with making money, and her mother ...",
+      title: 'The Poor Little Rich Girl'
+    },
+    {
+      plot: "A greedy tycoon decides, on a whim, to corner the world market in wheat. This doubles the price of bread, forcing the grain's producers into charity lines and further into poverty. The film...",
+      title: 'A Corner in Wheat'
+    },
+    {
+      plot: 'A penniless young man tries to save an heiress from kidnappers and help her secure her inheritance.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BNzE1OWRlNDgtMTllNi00NTZiLWIyNTktYTk0MDY1ZWUwYTc5XkEyXkFqcGdeQXVyMjUxODE0MDY@._V1_SY1000_SX677_AL_.jpg',
+      title: 'From Hand to Mouth'
+    },
+    {
+      plot: "A District Attorney's outspoken stand on abortion gets him in trouble with the local community.",
+      title: 'Where Are My Children?'
+    },
+    {
+      plot: "A newly wedded couple attempt to build a house with a prefabricated kit, unaware that a rival sabotaged the kit's component numbering.",
+      title: 'One Week'
+    },
+    {
+      plot: 'A tipsy doctor encounters his patient sleepwalking on a building ledge, high above the street.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BODliMjc3ODctYjhlOC00MDM5LTgzNmUtMjQ1MmViNDQ0NzlhXkEyXkFqcGdeQXVyNTM3MDMyMDQ@._V1_SY1000_SX677_AL_.jpg',
+      title: 'High and Dizzy'
+    },
+    {
+      plot: `A rich young Easterner who has always wanted to live in "the Wild West" plans to move to a Western town. Unknown to him, the town's "wild" days are long gone, and it is an orderly, ...`,
+      title: 'Wild and Woolly'
+    },
+    {
+      plot: 'An extended family split up in France and Germany find themselves on opposing sides of the battlefield during World War I.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BOTU1ODQyYTctODhkNy00YWRmLWE2YzYtMTQ5ZjA3OTNiN2QyXkEyXkFqcGdeQXVyMzE0MjY5ODA@._V1_SY1000_SX677_AL_.jpg',
+      title: 'The Four Horsemen of the Apocalypse'
+    },
+    {
+      plot: 'A young man, unaccustomed to children, must accompany a young girl on a train trip.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BYjgzYzY1NjEtYWQxZS00ZjA4LWJlYmQtYzRjNTg3NjUwNDRlXkEyXkFqcGdeQXVyMjUxODE0MDY@._V1_SY1000_SX677_AL_.jpg',
+      title: 'Now or Never'
+    },
+    {
+      plot: 'A woman, with the aid of her police officer sweetheart, endeavors to uncover the prostitution ring that has kidnapped her sister, and the philanthropist who secretly runs it.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BYzk0YWQzMGYtYTM5MC00NjM2LWE5YzYtMjgyNDVhZDg1N2YzXkEyXkFqcGdeQXVyMzE0MjY5ODA@._V1_SY1000_SX677_AL_.jpg',
+      title: 'Traffic in Souls'
+    },
+    {
+      plot: 'A group of bandits stage a brazen train hold-up, only to find a determined posse hot on their heels.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BMTU3NjE5NzYtYTYyNS00MDVmLWIwYjgtMmYwYWIxZDYyNzU2XkEyXkFqcGdeQXVyNzQzNzQxNzI@._V1_SY1000_SX677_AL_.jpg',
+      title: 'The Great Train Robbery'
+    },
+    {
+      plot: 'Original advertising for the film describes it as a drama of primitive life on the shores of the North Pacific...',
+      poster: 'https://m.media-amazon.com/images/M/MV5BMjE3MjAyNzM5NV5BMl5BanBnXkFtZTgwMjA5OTg5NjE@._V1_SY1000_SX677_AL_.jpg',
+      title: 'In the Land of the Head Hunters'
+    },
+    {
+      plot: 'A romantic rivalry among members of a secret society becomes even tenser when one of the men is assigned to carry out an assassination.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BMTkwOTUyNDk1N15BMl5BanBnXkFtZTYwODI3MzI3._V1_SY1000_SX677_AL_.jpg',
+      title: 'The Ace of Hearts'
+    },
+    {
+      plot: 'At 10 years old, Owens becomes a ragged orphan when his sainted mother dies. The Conways, who are next door neighbors, take Owen in, but the constant drinking by Jim soon puts Owen on the ...',
+      poster: 'https://m.media-amazon.com/images/M/MV5BNDkxZGU4NmMtODJlNy00YzA2LTg4ZGMtNGFlNzAyNzcxOTM1XkEyXkFqcGdeQXVyOTM3MjcyMjI@._V1_SY1000_SX677_AL_.jpg',
+      title: 'Regeneration'
+    },
+    {
+      plot: 'Two peasant children, Mytyl and Tyltyl, are led by Berylune, a fairy, to search for the Blue Bird of Happiness. Berylune gives Tyltyl a cap with a diamond setting, and when Tyltyl turns the...',
+      poster: 'https://m.media-amazon.com/images/M/MV5BMjNlMThmNzItMTZlMS00YzJkLTk1MzktYzIyMzllOGFmZmRlXkEyXkFqcGdeQXVyMzE0MjY5ODA@._V1_SY1000_SX677_AL_.jpg',
+      title: 'The Blue Bird'
+    },
+    {
+      plot: 'When three thuggish men are responsible for the death of his father and the crippling of his brother, young David must choose between supporting his family or risking his life and exacting vengeance.',
+      poster: 'https://m.media-amazon.com/images/M/MV5BMjIwMzA3MDAyNl5BMl5BanBnXkFtZTgwNzMyNTE1MjE@._V1_SY1000_SX677_AL_.jpg',
+      title: "Tol'able David"
+    }
+  ];
+  const contenedor=document.getElementById("contenedorPelicula");
+    var contenidoPeliculas="";
+  for (pelicula of movies){
+    contenidoPeliculas=contenidoPeliculas +`
+    <div class="col">
+          <div class="card">
+            <img src= ${pelicula.poster} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+    `
+  }
+  contenedor.innerHTML=contenidoPeliculas;
